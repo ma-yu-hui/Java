575b8c4d29640d09f1cbb0d3ea84dda6265d8d62
myh <2722407149@qq.com>
2022-10-09 22:31:49 +0800

    10.9号的代码

--- Java_first_capter/src/com/atguigu/first/val2.java
+++ Java_first_capter/src/com/atguigu/first/val2.java
@@ -3,18 +3,19 @@ package com.atguigu.first;
 /**
  * @Description: // 类说明，在创建类时要填写
  * @ClassName: val2    // 类名，会自动填充
- * @Author: MYH          // 创建者
+ * @Author: MYH           // 创建者
  * @Date: 2022/9/15 21:33   // 时间
  * @Version: 1.0     // 版本
  */
 public class val2 {
     public static void main(String[] args) {
-        for (int i = 0; i <= 5; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
-                System.out.print("\t");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j <= 10 ; j++) {
+                if (j % 4 == 0){
+                    return;
+                }
+                System.out.print(j);
             }
-            System.out.println();
             System.out.println();
         }
     }
